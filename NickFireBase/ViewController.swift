fcf70516cbf180de5c08f94e44f1b37cbed909cb
Nick Liu <hybrida666@icloud.com>
2023-05-22 14:43:29 +0800

    Initial Commit

---
+++ NickFireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NickFireBase
+//
+//  Created by Nick Liu on 2023/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
