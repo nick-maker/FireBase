02833e1ecc36ffb0e0f3313dc3c3ff59a1f83387
Nick Liu <hybrida666@icloud.com>
2023-05-23 00:40:06 +0800

    initial commit

--- NickFireBase/ViewController.swift
+++ NickFireBase/ViewController.swift
@@ -6,14 +6,121 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var contentField: UITextField!
+    
+    @IBOutlet weak var beautyButton: UIButton!
+    
+    @IBOutlet weak var gossipButton: UIButton!
+    
+    @IBOutlet weak var schoolLife: UIButton!
+    
+    @IBOutlet weak var publishButton: UIButton!
+    
+    @IBAction func tagTapped(_ sender: UIButton) {
+        beautyButton.isEnabled = false
+        gossipButton.isEnabled = false
+        schoolLife.isEnabled = false
+        sender.isEnabled = true
+        if sender == beautyButton {
+                tag = "Beauty"
+            } else if sender == gossipButton {
+                tag = "Gossiping"
+            } else if sender == schoolLife {
+                tag = "SchoolLife"
+            } else {
+                tag = nil
+            }
+        contentField.resignFirstResponder()
+    }
+    
+    @IBAction func publishAction(_ sender: UIButton) {
+        addData()
+//        getData()
+        
+        titleTextField.text = ""
+        contentField.text = ""
+        beautyButton.isEnabled = true
+        gossipButton.isEnabled = true
+        schoolLife.isEnabled = true
+        
+    }
+    
+    let db = Firestore.firestore()
+    var titleText: String?
+    var contentText: String?
+    var tag: String?
+    
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        titleTextField.delegate = self
+        contentField.delegate = self
+        listen()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        titleText = titleTextField.text
+        contentText = contentField.text
     }
 
+    func addData() {
+        let newRef = db.collection("posts").document()
+        newRef.setData([
+            "id": newRef.documentID,
+            "title": titleText!,
+            "content": contentText!,
+            "tag": tag!,
+            "author_id": "hybrida666",
+            "created_time": FieldValue.serverTimestamp()
+        ]) { err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+            }
+        }
+    }
+    
+    func getData() {
+        db.collection("posts").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.data())")
+                }
+            }
+        }
+    }
+    
+    func listen() {
+        db.collection("posts").addSnapshotListener { querySnapshot, error in
+            if let error = error {
+                print("Error listening for changes: \(error)")
+                return
+            }
+            
+            guard let documents = querySnapshot?.documents else {
+                print("No documents available")
+                return
+            }
+            
+            for document in documents {
+                print("\(document.data())")
+            }
+        }
+    }
+    
 
+    
 }
 
